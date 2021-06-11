757c11908c9b3790583fd757d0f6317b1ea3bf82
surajmt8848 <suraj8848mt@gmail.com>
2021-06-11 14:14:40 +0545

    room host urls

--- config/hosts.py
+++ config/hosts.py
@@ -7,5 +7,6 @@ host_patterns = patterns(
     host(r"www", settings.ROOT_URLCONF, name="www"),
     host(r"api", "config.urls.hotel", name="hotels"),
     host(r"api", "config.urls.common", name="common"),
+    host(r"api", "config.urls.room", name="room"),
     host(r"dj-admin", "config.urls.dj-admin", name="dj-admin"),
 )
