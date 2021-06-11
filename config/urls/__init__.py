73fa16711b06cb555677096a1e62af493b1b6d95
surajmt8848 <suraj8848mt@gmail.com>
2021-06-11 14:15:00 +0545

    room ursl

--- config/urls/__init__.py
+++ config/urls/__init__.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("dj-admin/", include("config.urls.dj-admin")),
     path("api/v1/hotel/", include("config.urls.hotel")),
     path("api/v1/common/", include("config.urls.common")),
+    path("api/v1/", include("config.urls.room")),
 ]
