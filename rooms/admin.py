7b44f85b7ded9993ddb70180e86eae736a8ae477
surajmt8848 <suraj8848mt@gmail.com>
2021-06-11 14:15:20 +0545

    room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import module_loading
 from . import models
 
 
@@ -9,3 +10,7 @@ class RoomAdmin(admin.ModelAdmin):
     list_filter = ("name",)
     search_fields = ("name__startswith",)
     list_display_links = ("name", "price")
+
+
+admin.site.register(models.Service_spec)
+admin.site.register(models.RoomService)
